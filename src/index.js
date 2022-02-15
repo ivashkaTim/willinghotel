d6ff8c75de71772f0532375f46f27722db8e56ed
ivashkaTim <mr.timofeev1995@mail.ru>
2022-02-15 19:06:26 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,36 +1,10 @@
 import './styles/index.scss';
 
-// import Swiper, { Navigation, Pagination } from 'swiper';
-// Swiper.use([Navigation, Pagination]);
-
-// import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
-
 import openBurgerMenu from './modules/openBurgerMenu';
 import changeLinkActive from './modules/changeLinkActive';
 import openSelect from './modules/openSelect';
 
 openBurgerMenu();
 changeLinkActive('.tabs', 'tabs__button', 'tabs__button--active');
-changeLinkActive('.hall-conference__list', 'hall-conference__link', 'hall-conference__link--active');
+changeLinkActive('.tabs-nav__list', 'tabs-nav__item', 'is-active');
 openSelect();
-// const swiper = new Swiper('.swiper', {
-//   slidesPerView: 1,
-//   spaceBetween: 16,
-//   loop: true,
-//   navigation: {
-//     nextEl: '.button_next',
-//     prevEl: '.button_prev',
-//   },
-//   breakpoints: {
-//     600: {
-//       slidesPerView: 2,
-//       spaceBetween: 36,
-//     },
-//     1000: {
-//       slidesPerView: 3,
-//       spaceBetween: 36,
-//     },
-//   },
-// });
--- src/modules/openBurgerMenu.js
+++ src/modules/openBurgerMenu.js
@@ -2,17 +2,20 @@ const openBurgerMenu = () =>{
 	const burgerMenu = document.querySelector('.hamburger ')
 	const headerMenu = document.querySelector('.header__menu')
 	const links = document.querySelectorAll('.nav__link')
+	const body = document.querySelector('body')
 
 
 	burgerMenu.addEventListener('click', ()=> {
 		burgerMenu.classList.toggle('hamburger_active')
 		headerMenu.classList.toggle('header__menu--active')
+		body.classList.toggle('no-scroll')
 	})
 
 	links.forEach(link => {
 		link.addEventListener('click', () => {
 			burgerMenu.classList.toggle('hamburger_active')
 			headerMenu.classList.toggle('header__menu--active')
+			body.classList.toggle('no-scroll')
 		})
 	})
 
